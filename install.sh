ab791bcd47d370bb0889c1c282e9902902673edb
sneivandt <sneivandt@users.noreply.github.com>
2022-08-07 14:01:22 -0700

    github codespaces symlink

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 # https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-github-codespaces-for-your-account#dotfiles
+
+# Install dotfiles
 ./dotfiles.sh -Ig
+
+# Configure vscode remote
+mkdir -pv ~/.vscode-remote/data/Machine/
+rm -rvf ~/.vscode-remote/data/Machine/settings.json
+ln -snvf /workspaces/.codespaces/.persistedshare/dotfiles/env/base-gui/symlinks/config/Code/User/settings.json ~/.vscode-remote/data/Machine/settings.json

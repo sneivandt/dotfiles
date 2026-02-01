8ad456eda54f96f69ed17fe7cb59f3552ea7ccc4
sneivandt <sneivandt@users.noreply.github.com>
2026-01-31 19:36:23 -0800

    volume

--- symlinks/config/volume/init-volume.sh
+++ symlinks/config/volume/init-volume.sh
@@ -4,8 +4,8 @@ set -o nounset
 
 # Unmute and set Master volume
 # Set Master to a reasonable default, so it acts as the main gain control.
-amixer -q sset Master unmute
-amixer -q sset Master 70%
+amixer -q sset Master unmute 2>/dev/null || true
+amixer -q sset Master 70% 2>/dev/null || true
 
 # Unmute and maximize secondary output channels.
 # By setting these to 100% (0dB), we ensure 'Master' dictates the actual output level.

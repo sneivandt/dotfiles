b40a0775d7a268ccf6b06b80dcb892ddf4d98c44
Test User <test@example.com>
2026-04-18 13:08:03 -0700

    full

--- symlinks/vim/lua/lazy-bootstrap.lua
+++ symlinks/vim/lua/lazy-bootstrap.lua
@@ -126,7 +126,7 @@ require("lazy").setup({
         tabline = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = { { "tabs", mode = 1, max_length = vim.o.columns } },
+          lualine_c = { { "tabs", mode = 1, path = 3, max_length = vim.o.columns } },
           lualine_x = {},
           lualine_y = {},
           lualine_z = {}
